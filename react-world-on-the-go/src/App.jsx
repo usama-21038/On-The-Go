0a8c5e83abdc35566fe4450b738ebcb2d6bfa120
Usama <shihabhasib777@gmail.com>
2026-01-13 02:56:30 +0600

    add a new component

--- react-world-on-the-go/src/App.jsx
+++ react-world-on-the-go/src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
+import './App.css'
+import Countries from './components/countries'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
      
-      <h1>React world on the Go...</h1>
+      <Countries></Countries>
      
     </>
   )
--- react-world-on-the-go/src/components/countries.jsx
+++ react-world-on-the-go/src/components/countries.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const countries = () => {
     return (
         <div>
-            <h2></h2>
+             <h2>Traveling Countries</h2>
         </div>
     );
 };
